94415a2876cd73e358af2cf4c33f0e8f5da845db
Kuniwak <orga.chem.job@gmail.com>
2019-10-19 16:34:38 +0900

    Support interleave

--- main.pl
+++ main.pl
@@ -1,32 +1,41 @@
-:- table transit_transitive/8.
+:- table transit_composition_transitive/8.
 
-deadlock(L_n, X_n, T1_n, T2_n) :-
+% TODO: SVars = (X_n, T1_n, T2_n)
+
+reachable(Ls_n, X_n, T1_n, T2_n) :-
+    init_locations(Ls),
     init_vars(X, T1, T2),
-    transit_transitive('P0', L_n, X, T1, T2, X_n, T1_n, T2_n),
-    \+ valid_end(L_n),
-    \+ transit(_, L_n, _, X_n, T1_n, T2_n, _, _, _).
+    transit_composition_transitive(Ls, Ls_n, X, T1, T2, X_n, T1_n, T2_n).
 
-transit_transitive(L, L_1, X, T1, T2, X_1, T1_1, T2_1) :- transit(_, L, L_1, X, T1, T2, X_1, T1_1, T2_1).
-transit_transitive(L, L_n, X, T1, T2, X_n, T1_n, T2_n) :-
-    transit(_, L, L_1, X, T1, T2, X_1, T1_1, T2_1),
-    transit_transitive(L_1, L_n, X_1, T1_1, T2_1, X_n, T1_n, T2_n).
+deadlock(Ls_n, X_n, T1_n, T2_n) :-
+    reachable(Ls_n, X_n, T1_n, T2_n),
+    \+ transit_composition(_, Ls_n, _, X_n, T1_n, T2_n, _, _, _).
+
+transit_composition_transitive(Ls, Ls_1, X, T1, T2, X_1, T1_1, T2_1) :-
+    transit_composition(_, Ls, Ls_1, X, T1, T2, X_1, T1_1, T2_1).
+transit_composition_transitive(Ls, Ls_n, X, T1, T2, X_n, T1_n, T2_n) :-
+    transit_composition(_, Ls, Ls_1, X, T1, T2, X_1, T1_1, T2_1),
+    transit_composition_transitive(Ls_1, Ls_n, X_1, T1_1, T2_1, X_n, T1_n, T2_n).
+
+transit_composition(T, Ls, Ls_1, X, T1, T2, X_1, T1_1, T2_1) :-
+    select(L, Ls, L_1, Ls_1),
+    transit(T, L, L_1, X, T1, T2, X_1, T1_1, T2_1).
 
 write_digraph :-
     writeln("strict digraph {"),
-    findall((T, L0, L1), (transit(T, L0, L1, _, _, _, _, _, _), write_edge(T, L0, L1)), _),
+    findall((T, Ls0, Ls1), (transit_composition(T, Ls0, Ls1, _, _, _, _, _, _), write_edge(T, Ls0, Ls1)), _),
     writeln("}").
 
 write_edge(T, L0, L1) :- write("  "), write(L0), write("->"), write(L1), write(" [label = "), write(T), writeln("]").
 
 % ---- Target specific ----
 
+init_locations(['P0']).
 init_vars(0, 0, 0).
 
 location('P0').
 location('P1').
 location('P3').
-
-valid_end('P3').
 
 transit(T, L0, L1, X, T1, T2, X_1, T1_1, T2_1) :-
     transit_read_1(T, L0, L1, X, T1, T2, X_1, T1_1, T2_1);
